2810b55b71389324f042a90f5d05a75cebe1d741
Igor <nielimbo@gmail.com>
2024-10-08 17:00:34 +0300

    Добавление функций filter и sort

--- src/processing.py
+++ src/processing.py
@@ -14,3 +14,7 @@ def filter_by_state(list_dict: list, state: str='EXECUTED') -> list:
             filtered_list.append(item)
     return filtered_list
 
+def sort_by_date(list_dict: list, type_sort: bool=True) -> list:
+    '''Функция, сортировки списка словарей по дате'''
+    return sorted(list_dict, key=lambda x: x['date'], reverse=type_sort)
+
