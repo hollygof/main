8da99076cb9228932a91e861e858f019024d39ca
Igor <nielimbo@gmail.com>
2024-10-08 18:11:01 +0300

    Исправление ошибок

--- src/processing.py
+++ src/processing.py
@@ -6,15 +6,16 @@ list_dict = [
     {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
 ]
 
-def filter_by_state(list_dict: list, state: str='EXECUTED') -> list:
-    '''Функция, выозврата словарей по ссответсвующиму ключу state'''
+
+def filter_by_state(list_dict: list, state: str = 'EXECUTED') -> list:
+    """Функция, выозврата словарей по ссответсвующиму ключу state"""
     filtered_list = []
     for item in list_dict:
         if item.get('state') == state:
             filtered_list.append(item)
     return filtered_list
 
-def sort_by_date(list_dict: list, type_sort: bool=True) -> list:
-    '''Функция, сортировки списка словарей по дате'''
-    return sorted(list_dict, key=lambda x: x['date'], reverse=type_sort)
 
+def sort_by_date(list_dict: list, type_sort: bool = True) -> list:
+    """Функция, сортировки списка словарей по дате"""
+    return sorted(list_dict, key=lambda x: x['date'], reverse=type_sort)
